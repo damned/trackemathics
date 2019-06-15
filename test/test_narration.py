e5340210ec3cbbff46f237011bb55d3ff01b1b47
damned <writetodan@yahoo.com>
2019-06-15 02:25:57 +0100

    value knows where it comes from

--- test/test_narration.py
+++ test/test_narration.py
@@ -2,4 +2,7 @@ import pytest
 from trackemathics.value import Value
 
 def test_value_can_tell_us_about_itself():
-    assert Value(2).narrate() == 'i am 2. i am who i am'
+    assert Value(2).narrate() == 'i am 2. i am who i am.'
+
+def test_value_can_tell_us_about_where_it_comes_from():
+    assert Value(3, 'bob').narrate() == 'i am 3. from: bob'
--- trackemathics/value.py
+++ trackemathics/value.py
@@ -1,6 +1,14 @@
 class Value:
-    def __init__(self, value):
+    def __init__(self, value, source=None):
         self.value = value
+        self.source = source
+
+    @property
+    def roots(self):
+        if (self.source):
+            return f"from: {self.source}"
+
+        return 'i am who i am.'
 
     def narrate(self):
-        return f"i am {self.value}. i am who i am"
+        return f"i am {self.value}. {self.roots}"

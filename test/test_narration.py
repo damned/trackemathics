1f185cd988c2b77cb2ab3044e9f736ffa2314a21
damned <writetodan@yahoo.com>
2019-06-15 02:37:25 +0100

    have a complex source

--- test/test_narration.py
+++ test/test_narration.py
@@ -6,3 +6,15 @@ def test_value_can_tell_us_about_itself():
 
 def test_value_can_tell_us_about_where_it_comes_from():
     assert Value(3, 'bob').narrate() == 'i am 3. from: bob'
+
+class Bingo:
+    def __init__(self, nickname):
+        self.nickname = nickname
+
+    def __repr__(self):
+        return f"the bingo number, {self.nickname}"
+
+def test_value_can_have_an_object_source():
+    bingo_four = Value(4, Bingo('knock at the door'))
+    assert bingo_four.narrate() == 'i am 4. from: the bingo number, knock at the door'
+
